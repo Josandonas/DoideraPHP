d054b52db43e7895b9fa53d3eb42852c8bcab6f3
Josandonas <josandonas@outlook.com>
2021-05-11 12:54:33 -0400

    adicionando modal

--- View/PaginaDados.php
+++ View/PaginaDados.php
@@ -13,6 +13,9 @@
 		<link href="../View/IconFont/icofont.min.css" rel="stylesheet">
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 	</head>
 
 	<body class="Dados">
@@ -41,20 +44,39 @@
 					<!-- mostra o qu está contido no array do banco de dados contruindo uma tabela com os dados internos como a tag <td> </td> e <tr> </tr>-->
 						<td><?php echo $mostrar["nome"]; ?></td>
 						<td><?php echo $mostrar["genero"]; ?></td>
-						<td><?php echo $mostrar["sobre"]; ?></td>
-						
-						<!-- cria dois botões responsaveis por retornar o id correspondente a um determinado animal no caso que são bem uteis para as funções de exclusão e edição-->
+						<td align="center"><button type="button"
+									class="btn btn-primary"
+									data-toggle="modal"
+									data-target="#ha<?php echo $mostrar["id_name"]; ?>">Ler <i class="icofont-eye"></i></button></td>
+
+							<div class="modal fade" id="ha<?php echo $mostrar["id_name"]; ?>" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-hidden="true">
+								<div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+									<div class="modal-content">
+									<div class="modal-header" >
+										<h5 class="modal-title" id="staticBackdropLabel"><?php echo $mostrar["nome"]; ?></h5>
+									</div>
+									<div class="modal-body">
+									<?php echo $mostrar["sobre"]; ?>
+									</div>
+									<div class="modal-footer">
+										<button type="button" class="btn btn-danger" data-dismiss="modal">Fechar</button>
+									</div>
+									</div>
+								</div>
+							</div>
+
 						<td class="text-center"> <a href="./PaginaEdicao.php ?codigo=<?php echo $mostrar["id_name"]; ?>" type="button" class="btn btn-warning"> <i class="icofont-ui-edit"></i> Editar </a> 
 						<a href="../Controller/Excluir.php ?codigo=<?php echo $mostrar["id_name"]; ?>" type="button" class="btn btn-danger"><i class="icofont-ui-delete"></i> Excluir </a> 
 						</td>
 					
 					</tr>
 				</tbody>
-		<?php  } ?>	
-		<!-- foi necessario criar um botão que me permitisse voltar a pagina de cadastro-->
+		<?php  } ?>
 			</table>
 		</div>
 	</body>
+	
 </html>
+
 <!-- onde a aplcação do mysql se encerra -->
 <?php   mysqli_close($con);?>
